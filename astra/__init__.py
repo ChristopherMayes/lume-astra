9eebb747362bfd787199c3f47530c7af2befc43c
ChristopherMayes <31023527+ChristopherMayes@users.noreply.github.com>
2021-09-15 12:04:25 -0700

    Move templates/ to examples/templates and updated all notebooks

--- astra/__init__.py
+++ astra/__init__.py
@@ -3,10 +3,5 @@ from .generator import AstraGenerator
 from .evaluate import evaluate_astra_with_generator
 from .astra_distgen import run_astra_with_distgen, evaluate_astra_with_distgen
 
-import os
-# Used to access data directory
-root, _ = os.path.split(__file__)
-template_dir = os.path.join(root, '../templates/')
-
 from . import _version
 __version__ = _version.get_versions()['version']
